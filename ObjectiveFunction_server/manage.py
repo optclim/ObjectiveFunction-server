f3556b0f27cbd98f4bebaadbf723f9e253b58dcd
Magnus Hagdorn <Magnus.Hagdorn@marsupium.org>
2022-06-14 14:38:41 +0100

    add option to drop all tables

--- ObjectiveFunction_server/manage.py
+++ ObjectiveFunction_server/manage.py
@@ -13,13 +13,16 @@ def main():
                         help="the password of the application")
     parser.add_argument('--init-db', action='store_true',
                         default=False, help="initialise database")
+    parser.add_argument('--delete-db', action='store_true',
+                        default=False, help="delete database")
 
     args = parser.parse_args()
 
     if args.init_db:
         db.create_all()
-
-    if args.app is not None:
+    elif args.delete_db:
+        db.drop_all()
+    elif args.app is not None:
         if args.password is None:
             parser.error('need to set password')
             sys.exit(1)
